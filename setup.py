cba09331aec9222397f34b1b8de4eb870b06afb3
Unknown <T.S.JaspersFocks@student.tudelft.nl>
2018-05-03 15:59:10 +0200

    update versionl

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 setup(
     name='plebnet',
 
-    version='0.1.1',
+    version='0.1.2',
 
     description='Working class botnet',
     long_description=long_description,
