7a72f40b766d84171b9dc359eddb51acd81c28f4
Unknown <T.S.JaspersFocks@student.tudelft.nl>
2018-05-04 15:00:04 +0200

    added logger

--- plebnet/utilities/logger.py
+++ plebnet/utilities/logger.py
@@ -1,11 +1,29 @@
+import logging
 
 
-def log(type, subtype, msg):
-    print(fill(type, 10) + " : " + fill(subtype, 10) + " : " + msg)
+class Logger(object):
+    def __init__(self, name="logger", file="/root/Documents/logs"):
+        self.logger = logging.getLogger(name)
+        self.logger.setLevel(logging.info)
+
+        self.handler = logging.FileHandler(file)
+        self.handler.setLevel(logging.INFO)
+
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        self.handler.setFormatter(formatter)
+
+        self.logger.addHandler(self.handler)
+
+    def log(self, file, method, msg):
+        tex = fill(file, 10) + " : " + fill(method, 10) + " : " + msg
+        print(tex)
+        self.logger.info(tex)
 
 
 def fill(tex, l):
-    st = tex
-    while len(st) < l:
-        st = st + " "
-    return st
+    if len(tex) > l:
+        tex = tex.substring(0, l-3) + "..."
+    else:
+        while len(tex) < l:
+            tex = tex + " "
+    return tex
