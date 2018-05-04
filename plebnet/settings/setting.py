61247349e4f123fd7fac0c3daca707cf73d3a34d
Unknown <T.S.JaspersFocks@student.tudelft.nl>
2018-05-04 14:13:39 +0200

    cleaned file

--- plebnet/settings/setting.py
+++ plebnet/settings/setting.py
@@ -1,21 +1,22 @@
 from configparser import ConfigParser
 
+
 class Settings(object):
- 	def __init__(self,filename):
+	def __init__(self, filename):
 		self.settings = ConfigParser()
 		self.filename = filename
 
-    def load(self):
+	def load(self):
 #		if not os.path.exists(self.filename):
 #			logger.log("Settings","load","file not existing")
 #			return False
-		files = self.settings.read(self.filename,encoding='utf-8')
-		return len(files)>0
+		files = self.settings.read(self.filename, encoding='utf-8')
+		return len(files) > 0
 
-	def get(self,section,key):
+	def get(self, section, key):
 		return self.settings.get(section,key)
 
-	def set(self,section,key,value):
+	def set(self, section, key, value):
 		if not self.settings.has_section(section):
 			self.settings.add_section(section)
-		self.settings.set(section,key,value)
+		self.settings.set(section, key, value)
