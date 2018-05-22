ab079e17b3b2a40f70abc25ebbad7d92ab50e5f6
Viktor <v.wigmore@student.tudelft.nl>
2018-05-22 11:24:21 +0200

    added tests for DNA

--- tests/agent/test_dna.py
+++ tests/agent/test_dna.py
@@ -8,19 +8,28 @@ class TestDna(unittest.TestCase):
 
     def setUp(self):
         self.test_dna = DNA()
+        self.test_dna.rate = 0.005
+        self.test_dna.length = 0.0
+        self.test_dna.dictionary = {}
+        self.test_dna.vps = {}
 
     def tearDown(self):
-        self.test_dna = None
+        pass
 
     def test_add_provider(self):
-        DNA().add_provider("provider1")
-        self.assertEqual(DNA().vps, {'provider1': 0.5})
+        self.test_dna.add_provider("provider1")
+        self.assertEqual(self.test_dna.vps, {'provider1': 0.5})
 
     def test_remove_provider(self):
-        DNA().add_provider("provider1")
-        DNA().remove_provider("provider1")
-        self.assertEqual({}, DNA().vps)
+        self.test_dna.add_provider("provider1")
+        self.test_dna.remove_provider("provider1")
+        self.assertEqual({}, self.test_dna.vps)
 
+    def test_normalize(self):
+        self.test_dna.add_provider("provider1")
+        self.test_dna.normalize()
+        self.assertEqual({'provider1': 1}, self.test_dna.vps)
+        
 
 if __name__ == '__main__':
     unittest.main()
