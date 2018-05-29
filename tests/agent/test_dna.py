c02ce4177b0d6cf805f2461f4df83c74649b78ac
Viktor <v.wigmore@student.tudelft.nl>
2018-05-29 14:32:26 +0200

    more dna tests

--- tests/agent/test_dna.py
+++ tests/agent/test_dna.py
@@ -39,6 +39,31 @@ class TestDna(unittest.TestCase):
         self.test_dna.mutate("provider1")
         self.assertEqual({'provider1': 1.5}, self.test_dna.vps)
 
+    def test_demutate_false(self):
+        self.test_dna.add_provider("provider1")
+        self.assertFalse(self.test_dna.demutate("provider2"))
+
+    def test_demutate_enough(self):
+        self.test_dna.add_provider("provider1")
+        self.test_dna.rate = 0.5
+        self.test_dna.demutate("provider1")
+        self.assertEqual({'provider1': 0.0}, self.test_dna.vps)
+
+    def test_demutate_not_enough(self):
+        self.test_dna.add_provider("provider1")
+        self.test_dna.rate = 0.2
+        self.test_dna.demutate("provider1")
+        self.test_dna.rate = 0.4
+        self.test_dna.demutate("provider1")
+        self.assertEqual({'provider1': 0.3}, self.test_dna.vps)
+
+    def test_denormalize(self):
+        self.test_dna.add_provider("provider1")
+        self.test_dna.length = 10
+
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
