83e19efc9131f39fe367d5faa5baae3b5fb055f0
codesalad <nguyen094@gmail.com>
2018-06-11 11:14:48 +0000

    fixed some bugs, turned testnet off

--- plebnet/controllers/cloudomate_controller.py
+++ plebnet/controllers/cloudomate_controller.py
@@ -179,10 +179,13 @@ def place_offer(chosen_est_price, config):
         logger.log("No MB available")
         return False
     config.bump_offer_date()
-    config.set('last_offer', {'BTC': chosen_est_price, 'MB': available_mb})
+
+    coin = 'TBTC' if plebnet_settings.get_instance().wallets_testnet() else 'BTC'
+
+    config.set('last_offer', {coin: chosen_est_price, 'MB': available_mb})
     price_per_unit = max(0.0001, chosen_est_price / float(available_mb))
     return market_controller.put_ask(price=price_per_unit,
-                                     price_type='BTC',
+                                     price_type=coin,
                                      quantity=available_mb,
                                      quantity_type='MB',
                                      timeout=plebnet_settings.TIME_IN_HOUR)
