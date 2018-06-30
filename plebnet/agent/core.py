307684bdf56a745b14c1bb468d6021ecaa12705d
codesalad <nguyen094@gmail.com>
2018-06-30 13:55:06 +0000

    fix

--- plebnet/agent/core.py
+++ plebnet/agent/core.py
@@ -69,7 +69,7 @@ def setup(args):
     irc_handler.start_irc_client()
 
     if config.get('tree') == '':
-        logger.log("tree set to: " % settings.irc_nick())
+        logger.log("tree set to: %" % settings.irc_nick())
         config.set('tree', settings.irc_nick())
 
     logger.success("PlebNet is ready to roll!")
