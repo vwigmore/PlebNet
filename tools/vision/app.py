ea2174c3c7118ef9da2b197955ac38b0abfd4b79
codesalad <nguyen094@gmail.com>
2018-07-03 10:07:41 +0000

    autorefresh

--- tools/vision/app.py
+++ tools/vision/app.py
@@ -10,7 +10,7 @@ import json
 import logging
 from datetime import datetime
 
-logging.basicConfig(format="%(threadName)s:%(message)s", level='NOTSET')
+# logging.basicConfig(format="%(threadName)s:%(message)s", level='NOTSET')
 
 app = Flask(__name__)
 
